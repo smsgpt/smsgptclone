ac8311d727ce8fad1b486cb134c82994ed6fc321
AndeemenGaming <ijjlakhanmyname@gmail.com>
2025-05-29 11:44:11 +0530

    changing port

--- main.py
+++ main.py
@@ -163,4 +163,4 @@ def send_sms(to_number, message):
 # --- MAIN ---
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
